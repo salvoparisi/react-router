3718a9755b9b24e52e2064ee9f5525e5bbb4d13b
salvoparisi <psalvo065@gmail.com>
2024-11-25 14:47:02 +0100

    add posts

---
+++ src/components/posts.js
@@ -0,0 +1,44 @@
+const posts = [
+    {
+        id: "1",
+        image: "https://via.placeholder.com/600x400",
+        title: "Post 1",
+        description: "Questo è il primo post di esempio con un'immagine placeholder.",
+        category: "Tech",
+        tags: ["React", "JavaScript", "Frontend"],
+    },
+    {
+        id: "2",
+        image: "https://via.placeholder.com/600x400",
+        title: "Post 2",
+        description: "Il secondo post tratta argomenti di lifestyle e benessere.",
+        category: "Lifestyle",
+        tags: ["Fitness", "Wellness", "Health"],
+    },
+    {
+        id: "3",
+        image: "https://via.placeholder.com/600x400",
+        title: "Post 3",
+        description: "Un esempio di post educativo sui linguaggi di programmazione.",
+        category: "Education",
+        tags: ["Coding", "Learning", "Development"],
+    },
+    {
+        id: "4",
+        image: "https://via.placeholder.com/600x400",
+        title: "Post 4",
+        description: "Questo post esplora le ultime novità nel mondo della tecnologia.",
+        category: "News",
+        tags: ["Tech News", "Innovation", "Gadgets"],
+    },
+    {
+        id: "5",
+        image: "https://via.placeholder.com/600x400",
+        title: "Post 5",
+        description: "Un post sul viaggio e la scoperta di nuove culture.",
+        category: "Travel",
+        tags: ["Adventure", "Culture", "Destinations"],
+    },
+];
+
+export default posts
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,29 @@
+import posts from '../components/posts'
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function Posts() {
+    console.log(posts);
+
     return (
         <>
             <h1>Post Page</h1>
+            <div className="container">
+                <div className="row">
+                    {posts.map((post) => (
+                        <div className="col-md-4 pb-4" key={post.id}>
+                            <div className="card">
+                                <img src={post.image} alt={post.title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{post.title}</h5>
+                                    <p className="card-text" style={{ height: "44px" }}>{post.description}</p>
+                                    <a href="#" className="btn btn-primary">{post.category}</a>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
         </>
     )
 }
